f980ca45febe594d4ccf1b3e11c32822de87c81a
elena galka <elenas.galka@gmail.com>
2021-05-27 13:32:27 +0300

    Add Login Form

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import './App.scss';
 
 import Home from './components/Home/Home';
 import Appointments from './components/Appointments/Appointments';
+import Login from './components/Login/Login';
+
 
 export default function App(props) {
   const { history } = props;
@@ -17,6 +19,7 @@ export default function App(props) {
   return (
     <div className='App'>
       <Switch>
+        <Route history={history} path='/login' component={Login} />
         <Route history={history} path='/home' component={Home} />
         <Route history={history} path='/appointments' component={Appointments} />
         <Redirect from='/' to='/home' />
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,50 +1,47 @@
 import React, { useEffect, useState } from 'react';
 import './Login.scss';
-import Header from '../Header/Header';
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import nurse from '../../images/nurse.png';
 
-import React, { useState } from "react";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
-import "./Login.css";
 
 export default function Login() {
-  const [userName, setuserName] = useState("");
-  const [password, setPassword] = useState("");
+    const [userName, setUserName] = useState(null);
+    const [password, setPassword] = useState(null);
 
-  function validateForm() {
-    return userName.length > 0 && password.length > 0;
-  }
+    function validateForm() {
+        return userName?.length > 0 && password?.length > 0;
+    }
 
-  function handleSubmit(event) {
-    event.preventDefault();
-  }
+    function handleSubmit(event) {
+        event.preventDefault();
+    }
 
-  return (
-    <div className="Login">
-      <Form onSubmit={handleSubmit}>
-        <Form.Group size="lg" controlId="userName">
-          <Form.Label>User Name</Form.Label>
-          <Form.Control
-            autoFocus
-            type="userName"
-            value={userName}
-            onChange={(e) => setUserName(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group size="lg" controlId="password">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </Form.Group>
-        <Button block size="lg" type="submit" disabled={!validateForm()}>
-          Login
+    return (
+        <div className="Login">
+            <img src={nurse} className='Section-Icon' alt='nurse'/>
+            <Form onSubmit={handleSubmit}>
+                <Form.Group size="lg" controlId="userName">
+                    <Form.Label>User Name</Form.Label>
+                    <Form.Control
+                        autoFocus
+                        type="userName"
+                        value={userName}
+                        onChange={(e) => setUserName(e.target.value)}
+                    />
+                </Form.Group>
+                <Form.Group size="lg" controlId="password">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </Form.Group>
+                <Button block size="lg" type="submit" disabled={!validateForm()}>
+                    Login
         </Button>
-      </Form>
-    </div>
-  );
+            </Form>
+        </div>
+    );
 }
